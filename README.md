b4ef1852e45f151b0d9ef5f97a0fff761204cdbf
Jaayy019 <jacobsambor@gmail.com>
2026-03-09 12:23:02 -0400

    Custom icon implemented

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 # devteam2447
 
+How to start the frontend:
+
+1. cd into the frontend folder
+2. Do "npm install" to install node modules folder and dependencies
+3. Use "npm start", app launches on http://localhost:3000
+
+In the event of errors:
+
+Outdated or conflicting packages:
+1. "rm -rf node_modules"
+2. "npm install"
+If using powershell:
+1. "rmdir node_modules -Recurse -Force"
+2. "npm install"
+
+Once done: "npm start"
+
+
 3/4/2026 - 1:43 PM - "Basic React Map and Leaflet integration"
 I added the leaflet map and used the React Map, it does work but the marker icon is broken.
+
+3/9/2026 - 12:16 PM - "Custom icon implemented"
+Added a new custom icon that can be used for all future charging stations.
--- frontend/src/MapView.js
+++ frontend/src/MapView.js
@@ -1,9 +1,20 @@
 //Imports the necessary leaflet map components
 import { MapContainer, TileLayer, Marker, Popup, Circle } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import chargerIcon from "./icons/map-pin.svg"
+
+const chargeIcon = L.icon({
+
+  iconUrl: chargerIcon,
+  iconSize: [40, 40],      
+  iconAnchor: [20, 40],    
+  popupAnchor: [0, -40]
+
+});
 
 export default function MapView() {
-    
+
   return (
 
     // Makes the map container, basically just the HTML file but in javascript
@@ -17,9 +28,7 @@ export default function MapView() {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
-      <Circle center={[40, -80]} radius={500} pathOptions={{ color: "red" }} />
-
-      <Marker position={[50, -0.09]}>
+      <Marker position={[50, -0.09]} icon={chargeIcon}>
         <Popup>
           <b>Hello World!</b><br />I am a popup.
         </Popup>
